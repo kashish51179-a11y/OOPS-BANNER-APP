9ac2b18b9f6026b462b5645bf8d8212a8efc6e8a
kashish51179-a11y <kashish51179@gmail.com>
2026-03-04 22:26:53 +0530

    use case 2 print oops

---
+++ uc2.java
@@ -0,0 +1,12 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+public class uc2 {
+   public static void main(String[] args) {
+      System.out.println(" ***   ***  ****   ****");
+      System.out.println("** ** ** ** ** ** **   ");
+      System.out.println("** ** ** ** ** ** **   ");
+      System.out.println("** ** ** ** ****   ****");
+      System.out.println("** ** ** ** **       **");
+      System.out.println("** ** ** ** **       **");
+      System.out.println(" ***   ***  **     ****");
+   }
+}
